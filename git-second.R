8ff0f298bddeb8ab00c96f9f4f8f83b33c062d10
Ranjit Abraham <ranjit.abraham@gmail.com>
2021-07-09 17:31:54 +0530

    Added Janitor

--- git-second.R
+++ git-second.R
@@ -1,7 +1,9 @@
 ## When we create a file in RStudio and save it - it shows up on the right in Git.
 ## Next it has NOT been committed - So we click commit above the file
 # After putting a Commit message and Commoting we click the History tab
-# Next we add a line say library(tidyr) and look at history
+# Next we add a line say library(tidyr) and after doing commit and Commit message (Subject) look at history
+## We now see 3 versions
+## Next we delete the tidyr and replace it with janitor and commit.
 
-library(tidyr)
+library(janitor)
 
