e56c58fd69e53185979cb0e2fc90a998eb789974
Ranjit Abraham <ranjit.abraham@gmail.com>
2021-07-09 17:29:11 +0530

    Added tidyr package

--- git-second.R
+++ git-second.R
@@ -1,4 +1,7 @@
 ## When we create a file in RStudio and save it - it shows up on the right in Git.
+## Next it has NOT been committed - So we click commit above the file
+# After putting a Commit message and Commoting we click the History tab
+# Next we add a line say library(tidyr) and look at history
 
-
+library(tidyr)
 
